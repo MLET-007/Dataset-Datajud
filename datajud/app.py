fd70110c1d06e01eb67aa0e13ba2b917629499df
MLET-007 <165738023+MLET-007@users.noreply.github.com>
2024-10-01 20:13:06 -0400

    Update app.py

--- datajud/app.py
+++ datajud/app.py
@@ -4,11 +4,28 @@ from datajud.routers import predict, import_process
 from alembic.config import Config
 from alembic import command
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 settings = Settings()
 app = FastAPI()
 
+origins = [
+    "http://127.0.0.1:8090",  
+    "http://127.0.0.1:8000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount('/mkdocs', StaticFiles(directory='site', html=True), name='mkdocs')
+
 app.include_router(predict.router)
 app.include_router(import_process.router)
 
